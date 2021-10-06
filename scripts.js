387c2fac0dc18119b58fb185c8ed596b4925ec2f
Nandini Mehrotra <90271211+NandiniMeh@users.noreply.github.com>
2021-10-06 19:11:02 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const bird = document.getElementById("bird")
+const crash = document.getElementById("crash")
+function jump() {
+    if(bird.classList != "jump"){
+        bird.classList.add("jump");
+        setTimeout(function () {
+            bird.classList.remove("jump");
+        },300);
+    }
+}
+ 
+let collision = setInterval(function () {
+    let crashLeft = parseInt(
+        window.getComputedStyle(crash).getPropertyValue("left")
+      );
+    let birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+    if (crashLeft < 50 && crashLeft > 0 && birdTop >= 150) {
+      alert("Twitter has crashed! Game Over!");
+    }
+  }, 11);
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
